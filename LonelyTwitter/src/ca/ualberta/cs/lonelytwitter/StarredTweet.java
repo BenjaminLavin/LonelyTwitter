46f5bfdfe6c5866dda4efb408a8eee856e14dbfe
blavin <blavin@ualberta.ca>
2014-09-16 19:56:15 -0600

    Refactor project

--- LonelyTwitter/src/ca/ualberta/cs/lonelytwitter/StarredTweet.java
+++ LonelyTwitter/src/ca/ualberta/cs/lonelytwitter/StarredTweet.java
@@ -1,25 +1,21 @@
-/**
- * 
- */
 package ca.ualberta.cs.lonelytwitter;
 
-import java.io.Serializable;
 import java.util.Date;
 
+public class StarredTweet extends AbstractTweet{
 
-public class Tweet extends AbstractTweet implements Serializable {
-
-	public Tweet(Date tweetDate, String tweetBody) {
+	public StarredTweet(Date tweetDate, String tweetBody) {
 		super();
 		this.tweetDate = tweetDate;
 		this.tweetBody = tweetBody;
 	}
 
 	public String getTweetBody() {
-		return tweetBody;
+		return "\u2605 " + tweetBody;
 	}
 
 	public String toString() {
-		return tweetBody;
+		return getTweetBody();
 	}
+
 }
---
+++ LonelyTwitter/src/ca/ualberta/cs/lonelytwitter/data/DataNetworkManager.java
@@ -0,0 +1,20 @@
+package ca.ualberta.cs.lonelytwitter.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.ualberta.cs.lonelytwitter.AbstractTweet;
+
+public class DataNetworkManager implements IDataManager{
+
+	public ArrayList<AbstractTweet> loadTweets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void saveTweets(List<AbstractTweet> lts) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
